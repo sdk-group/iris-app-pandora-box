d08cb7035080af7b2532a9491399a083ba687442
artificial-life <proteus.protocol@gmail.com>
2016-03-17 13:04:59 +0600

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+## Polymer-based operator ARM
+
+Based on [Polymer Starter Kit](https://developers.google.com/web/tools/polymer-starter-kit/)
+
+##Install
+
+* Скачиваем проект как [zip](https://github.com/sdk-group/iris-app-control-panel/archive/master.zip)
+* Распаковываем
+* В консоли
+ ```` 
+/cd iris-app-control-panel
+
+npm install
+
+bower install
+
+gulp serve 
+````
+* После этих действий должен открыться браузер 
+
+##Workflow
+
+* SOON
+
---
+++ app/scripts/interface-init.js
@@ -0,0 +1,59 @@
+(function (document) {
+	'use strict';
+
+	var app = document.querySelector('#app');
+
+	// Sets app default base URL
+	app.baseUrl = '';
+	if (window.location.port === '') { // if production
+		app.baseUrl = '/production/oper/';
+	}
+
+	app.displayInstalledToast = function () {
+		// Check to make sure caching is actually enabled—it won't be in the dev environment.
+		if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
+			Polymer.dom(document).querySelector('#caching-complete').show();
+		}
+	};
+
+	app.addEventListener('dom-change', function () {
+		console.log('Our app is ready to rock!');
+	});
+
+	// See https://github.com/Polymer/polymer/issues/1381
+	window.addEventListener('WebComponentsReady', function () {
+		// imports are loaded and elements have been registered
+	});
+
+	// Scroll page to top and expand header
+	app.scrollPageToTop = function () {
+		app.$.headerPanelMain.scrollToTop(true);
+	};
+
+	app.closeDrawer = function () {
+		app.$.paperDrawerPanel.closeDrawer();
+	};
+
+	app.menuAction = function () {
+		app.$.paperDrawerPanel.togglePanel();
+	}
+
+	app.test_tree = {
+		ServiceGroup: {
+			"@id": {},
+			"@type": {},
+			"items_per_page": {},
+			"content": {},
+			"label": {},
+			"view_name": {}
+		},
+		Schedule: {
+			"@id": {},
+			"@type": {},
+			"has_day": {},
+			"has_time_description": {},
+			"has_owner": {}
+		}
+	};
+
+})(document);
